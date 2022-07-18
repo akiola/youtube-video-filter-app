4764061be1a8f75af04e4a581dd27b0a5c6759af
Godfred Awuku <awukugodfredglo81@gmail.com>
2022-07-18 05:26:42 +0000

    updated api code

--- project1.py
+++ project1.py
@@ -1,14 +1,46 @@
-import googleapiclient.discovery as API
-import os 
+import googleapiclient.discovery
+import json 
 
-API_KEY = os.environ.get("API_KEY")
+def videoSearch(max_limit, video_duration, search_keyword):
 
-youtube = API.build('youtube', 'v3', developerKey = API_KEY)
+    version = 'v3'
+    api_service = 'youtube'
+    api_key = 'AIzaSyA19Pf8pttQKAbebSgH5EVLiLXjYj8-sYo'
+    
+    final_video_data = {}
 
-request = youtube.channels().list(
-  part = "statistics", 
-  id = 'schafer5'
+    youtube = googleapiclient.discovery.build(api_service, version, developerKey = api_key)
+    request = youtube.search().list(
+    q= search_keyword, #input the key word search here 
+    part="id,snippet",
+    type = "video",
+    videoDuration = video_duration,
+    order = "viewCount",
+    videoEmbeddable = "true",
+    maxResults= max_limit
 )
+    
+    response = request.execute()
+    data = json.dumps(response, indent = 3)
+    
+    for i in range(max_limit):
+        
+        new = {}
+        new['video_id'] = response['items'][i]['id']["videoId"]
+        new['video_title'] = response['items'][i]['snippet']['title']
+        new['video_description'] = response['items'][i]['snippet']['description']
+        new['video_thumbnail'] = response['items'][i]['snippet']['thumbnails']['medium']['url']
+        new['video_link'] = 'https://youtu.be/'+response['items'][i]['id']["videoId"]
+        
+        
+        final_video_data['search_video'+str(i+1)] = new 
+    data = json.dumps(final_video_data, indent = 3)
+    return data
 
-response = request.execute()
-print(response)
+search_keyword = input("Enter the search keyword: ")
+max_limit = int(input("Enter the maximum search results: "))
+video_duration = input("Enter long or short:")
+print(videoSearch(max_limit, video_duration, search_keyword))
+        
+        
+
